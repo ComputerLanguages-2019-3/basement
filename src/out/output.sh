9c9fb693312b737e2346019b3933bfdf0e46e308
Laura <laura.c@uptimeanalytics.com>
2020-02-03 06:47:03 -0500

    :pencil: readme

--- src/out/output.sh
+++ src/out/output.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-docker stop test-postgres
-docker rm test-postgres
-docker run --name test-postgres -p 80:800 -e POSTGRES_USER="user" -e POSTGRES_PASSWORD="pass" -e POSTGRES_DB="db" -d postgres
-docker ps  | grep postgres
+docker stop postgres-container
+docker rm postgres-container
+docker run --name postgres-container -p 4001:5234 -e POSTGRES_USER="user" -e POSTGRES_PASSWORD="pass" -e POSTGRES_DB="db" -d postgres
