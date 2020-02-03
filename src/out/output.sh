a7edb3872d343cc840005fc93e5e66908a9b1003
Laura <laura.c@uptimeanalytics.com>
2020-02-02 21:01:05 -0500

    :sparkles: :rocket: basement jar

--- src/out/output.sh
+++ src/out/output.sh
@@ -2,4 +2,4 @@
 docker stop test-postgres
 docker rm test-postgres
 docker run --name test-postgres -p 80:800 -e POSTGRES_USER="user" -e POSTGRES_PASSWORD="pass" -e POSTGRES_DB="db" -d postgres
-docker ps  -a | grep superset | grep up
+docker ps  | grep postgres
